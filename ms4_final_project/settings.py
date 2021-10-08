42ca7dbb0b4084587f948a135e7df746654300af
Kpokc <pavmakarovs@gmail.com>
2021-10-08 13:17:31 +0000

    Added sort by categories logic

--- ms4_final_project/settings.py
+++ ms4_final_project/settings.py
@@ -105,7 +105,7 @@ WSGI_APPLICATION = 'ms4_final_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -11,20 +11,31 @@ def products(request):
 
     products = Product.objects.all()
     query = None
+    category = None
 
     if request.GET:
-        if 'q' in request.GET:
-            query = request.GET['q']
-            if not query:
-                messages.error(request, "You didn't enter any search criteria!")
-                return redirect(reverse('products'))
-            
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
-            products = products.filter(queries)
+        if request.GET:
+            if 'category' in request.GET:
+                category = request.GET['category'].split(',')
+                products = products.filter(category__name__in=category)
+                category = Category.objects.filter(name__in=category)
+
+                if 'q' in request.GET:
+                    print(request.GET['q'])
+
+            if 'q' in request.GET:
+                query = request.GET['q']
+                if not query:
+                    messages.error(request, "You didn't enter any search criteria!")
+                    return redirect(reverse('products'))
+
+                queries = Q(name__icontains=query) | Q(description__icontains=query)
+                products = products.filter(queries)
 
     context = {
         'products': products,
-        'searchterm' : query,
+        'searchterm': query,
+        'current_category': category,
     }
 
     return render(request, 'products/products.html', context)
