04e0dda63035d10ab32ebc7b64f088e18a3374f7
Kpokc <pavmakarovs@gmail.com>
2021-10-19 16:53:29 +0000

    Updated - bag template python variables updated

--- ms4_final_project/settings.py
+++ ms4_final_project/settings.py
@@ -28,12 +28,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
 
-# if 'DEVELOPMENT' in os.environ:
-#     DEBUG = False
-# else:
-#     DEBUG = True
+if 'DEVELOPMENT' in os.environ:
+    DEBUG = False
+else:
+    DEBUG = True
 
 ALLOWED_HOSTS = ['final-ms4-app.herokuapp.com', 'localhost']
 
