7b29a57f16a4932bc71a7d20451e9cee71a9404a
Kpokc <pavmakarovs@gmail.com>
2021-10-19 14:11:39 +0000

    Updated css mobile view

--- ms4_final_project/settings.py
+++ ms4_final_project/settings.py
@@ -24,11 +24,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', ')$q6ik5r!oc-16z2qoy=ho!)z5l+^($k+n&y4gr@0w7*f8_%^w')
+SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
+
+# if 'DEVELOPMENT' in os.environ:
+#     DEBUG = False
+# else:
+#     DEBUG = True
 
 ALLOWED_HOSTS = ['final-ms4-app.herokuapp.com', 'localhost']
 
@@ -176,7 +181,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -203,7 +207,6 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-
 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
