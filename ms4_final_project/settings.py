4aa29c342abe3f5a65b498cb1316e5907b52b90d
Kpokc <pavmakarovs@gmail.com>
2021-10-21 09:24:31 +0000

    Added - top menu button hover efects

--- ms4_final_project/settings.py
+++ ms4_final_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
+#DEBUG = 'DEVELOPMENT' in os.environ
 
 if 'DEVELOPMENT' in os.environ:
     DEBUG = False
