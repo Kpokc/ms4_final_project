cd4d39317e16cb21fdf221e0acbbae7a604cf026
Kpokc <pavmakarovs@gmail.com>
2021-10-27 21:51:40 +0000

    Updated allauth templates

--- checkout/views.py
+++ checkout/views.py
@@ -172,7 +172,6 @@ def checkout_success(request, order_number):
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
 
-
     if 'bag' in request.session:
         del request.session['bag']
 
