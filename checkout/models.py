64a209165943bd9491b5afbf10e7455722cbd7b9
Kpokc <pavmakarovs@gmail.com>
2021-11-04 14:40:59 +0000

    Searching for incorrect fiels size

--- checkout/models.py
+++ checkout/models.py
@@ -17,30 +17,39 @@ from profiles.models import UserProfile
 
 
 class Order(models.Model):
-    order_number = models.CharField(max_length=32, null=False, editable=False)
+    order_number = models.CharField(max_length=254, null=False, editable=False)  #32
     user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
                                      null=True, blank=True, related_name='orders')
-    full_name = models.CharField(max_length=50, null=False, blank=False)
+    full_name = models.CharField(max_length=254, null=False, blank=False)  #50
     email = models.EmailField(max_length=254, null=False, blank=False)
-    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    phone_number = models.CharField(max_length=254, null=False, blank=False)  # 20
+
     country = CountryField(blank_label='Country *', null=False, blank=False)
-    postcode = models.CharField(max_length=20, null=True, blank=True)
-    town_or_city = models.CharField(max_length=40, null=False, blank=False)
-    street_address1 = models.CharField(max_length=80, null=False, blank=False)
-    street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    county = models.CharField(max_length=80, null=True, blank=True)
+    
+    postcode = models.CharField(max_length=254, null=True, blank=True)  # 20
+    town_or_city = models.CharField(max_length=254, null=False, blank=False)  # 40
+    street_address1 = models.CharField(max_length=254, null=False, blank=False)  # 80
+    street_address2 = models.CharField(max_length=254, null=True, blank=True)  # 80
+    county = models.CharField(max_length=254, null=True, blank=True)  # 80
 
     # date = models.DateTimeField(auto_now_add=True)
     date = models.SlugField(default=datetime.now, blank=True)
 
     delivery_cost = models.SlugField(null=False, default=0)
     # delivery_cost = models.DecimalField(max_digits=6, decimal_places=4, null=False, default=0)
+
     order_total = models.SlugField(null=False, default=0)
     # order_total = models.DecimalField(max_digits=10, decimal_places=4, null=False, default=0)
+
     grand_total = models.SlugField(null=False, default=0)
     # grand_total = models.DecimalField(max_digits=10, decimal_places=4, null=False, default=0)
-    original_bag = models.TextField(null=False, blank=False, default='')
-    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+
+    original_bag = models.SlugField(null=False, default=0)
+    # original_bag = models.TextField(null=False, blank=False, default='')
+
+    stripe_pid = models.SlugField(null=False, default=0)
+    
+    # stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
