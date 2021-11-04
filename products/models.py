15250270502fd09943369f3cdcf3c129ad255d24
Kpokc <pavmakarovs@gmail.com>
2021-11-04 14:56:04 +0000

    Models.Products fields changed

--- products/models.py
+++ products/models.py
@@ -25,10 +25,13 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(
-        max_digits=6, decimal_places=2, null=True, blank=True
-        )
+    
+    price = models.SlugField(null=False, default=0)
+    # price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    rating = models.SlugField(null=False, default=0)
+    # rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+
     size = models.BooleanField(default=True, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
